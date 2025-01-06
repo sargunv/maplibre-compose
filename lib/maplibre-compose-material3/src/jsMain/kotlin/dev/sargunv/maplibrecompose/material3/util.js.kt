ad35693a92d3e3bb5b0affc867c8389fad8615a6
Tobias Zwick <newton@westnordost.de>
2025-01-06 22:38:20 +0100

    remove unused import

--- lib/maplibre-compose-material3/src/jsMain/kotlin/dev/sargunv/maplibrecompose/material3/util.js.kt
+++ lib/maplibre-compose-material3/src/jsMain/kotlin/dev/sargunv/maplibrecompose/material3/util.js.kt
@@ -1,7 +1,5 @@
 package dev.sargunv.maplibrecompose.material3
 
-import dev.sargunv.maplibrecompose.material3.controls.ScaleBarMeasure
-
 internal actual fun defaultLanguage(): String? =
   new Intl.Locale(navigator.language).language
 
