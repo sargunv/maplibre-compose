3ef44db802f6c18959d4ab970f5d14ed271ce641
Sargun Vohra <sargun.vohra@gmail.com>
2025-01-07 20:06:17 -0800

    update iOS todo

--- lib/maplibre-compose-material3/src/iosMain/kotlin/dev/sargunv/maplibrecompose/material3/util.ios.kt
+++ lib/maplibre-compose-material3/src/iosMain/kotlin/dev/sargunv/maplibrecompose/material3/util.ios.kt
@@ -6,23 +6,14 @@ import dev.sargunv.maplibrecompose.material3.controls.ScaleBarMeasure
 @Composable internal actual fun systemDefaultPrimaryMeasure(): ScaleBarMeasure? = null
 
 /*
-TODO iOS/Mac developer:
+TODO iOS developer: needs to be implemented in Swift with #available
 internal actual fun scaleBareMeasurePreference(): ScaleBarMeasure? {
-  // user locale also contains user preferences such as the measurement system
-  val userLocale = NSLocale.currentLocale
-  // and this locale doesn't
-  val defaultLocale = NSLocale(languageCode = locale.language, languageRegion = locale.region)
-
-  // ONLY return the user's measurement system setting if it deviates from the language+country
-  // default, i.e. the user actually explicitly changed this setting
-  if (userLocale.measurementSystem != defaultLocale.measurementSystem) {
-    return when (userlocale.measurementSystem) {
-      Locale.MeasurementSystem.metric -> ScaleBarMeasure.Metric
-      Locale.MeasurementSystem.uk -> ScaleBarMeasure.YardsAndMiles
-      Locale.MeasurementSystem.us -> ScaleBarMeasure.FeetAndMiles
-      else -> null
-    }
+  val system = NSLocale.currentLocale.measurementSystem
+  return when (userlocale.measurementSystem) {
+    Locale.MeasurementSystem.metric -> ScaleBarMeasure.Metric
+    Locale.MeasurementSystem.uk -> ScaleBarMeasure.YardsAndMiles
+    Locale.MeasurementSystem.us -> ScaleBarMeasure.FeetAndMiles
+    else -> null
   }
-  return null
 }
 */
