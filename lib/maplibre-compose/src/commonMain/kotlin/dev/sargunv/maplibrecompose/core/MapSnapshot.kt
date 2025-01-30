33da02469bdd92b999024071b862d48872809ec9
michalgwo <114614618+michalgwo@users.noreply.github.com>
2025-01-30 19:43:07 +0100

    removed unused class

--- lib/maplibre-compose/src/commonMain/kotlin/dev/sargunv/maplibrecompose/core/MapSnapshot.kt
+++
@@ -1,9 +0,0 @@
-package dev.sargunv.maplibrecompose.core
-
-import androidx.compose.ui.graphics.ImageBitmap
-
-public data class MapSnapshot(
-  val bitmap: ImageBitmap,
-  val attributions: List<String>,
-  val isShowLogo: Boolean,
-)
