c848c096a0ff6643146909232629da57b586d461
michalgwo <114614618+michalgwo@users.noreply.github.com>
2025-02-01 19:44:38 +0100

    docs

--- lib/maplibre-compose/src/commonMain/kotlin/dev/sargunv/maplibrecompose/core/Style.kt
+++ lib/maplibre-compose/src/commonMain/kotlin/dev/sargunv/maplibrecompose/core/Style.kt
@@ -5,32 +5,74 @@ import dev.sargunv.maplibrecompose.core.layer.Layer
 import dev.sargunv.maplibrecompose.core.source.Source
 
 internal interface Style {
+  /**
+   * Returns true if the style is loaded. Returns false if a new style is underway of being loaded.
+   */
   var isLoaded: Boolean
 
+  /**
+   * @throws IllegalStateException on Android if [isLoaded] is false.
+   */
   fun addImage(id: String, image: ImageBitmap, sdf: Boolean)
 
+  /**
+   * @throws IllegalStateException on Android if [isLoaded] is false.
+   */
   fun removeImage(id: String)
 
+  /**
+   * @throws IllegalStateException on Android if [isLoaded] is false.
+   */
   fun getSource(id: String): Source?
 
+  /**
+   * @throws IllegalStateException on Android if [isLoaded] is false.
+   */
   fun getSources(): List<Source>
 
+  /**
+   * @throws IllegalStateException on Android if [isLoaded] is false.
+   */
   fun addSource(source: Source)
 
+  /**
+   * @throws IllegalStateException on Android if [isLoaded] is false.
+   */
   fun removeSource(source: Source)
 
+  /**
+   * @throws IllegalStateException on Android if [isLoaded] is false.
+   */
   fun getLayer(id: String): Layer?
 
+  /**
+   * @throws IllegalStateException on Android if [isLoaded] is false.
+   */
   fun getLayers(): List<Layer>
 
+  /**
+   * @throws IllegalStateException on Android if [isLoaded] is false.
+   */
   fun addLayer(layer: Layer)
 
+  /**
+   * @throws IllegalStateException on Android if [isLoaded] is false.
+   */
   fun addLayerAbove(id: String, layer: Layer)
 
+  /**
+   * @throws IllegalStateException on Android if [isLoaded] is false.
+   */
   fun addLayerBelow(id: String, layer: Layer)
 
+  /**
+   * @throws IllegalStateException on Android if [isLoaded] is false.
+   */
   fun addLayerAt(index: Int, layer: Layer)
 
+  /**
+   * @throws IllegalStateException on Android if [isLoaded] is false.
+   */
   fun removeLayer(layer: Layer)
 
   object Null : Style {
