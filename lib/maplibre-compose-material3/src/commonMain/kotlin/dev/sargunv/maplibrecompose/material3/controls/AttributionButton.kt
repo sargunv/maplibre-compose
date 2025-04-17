33c39ef493a2e3dfb571182412366b5bdce04b4c
Michał Gwóźdź <114614618+michalgwo@users.noreply.github.com>
2025-04-17 18:52:38 +0200

    Attribution Button improvements (#307)

--- lib/maplibre-compose-material3/src/commonMain/kotlin/dev/sargunv/maplibrecompose/material3/controls/AttributionButton.kt
+++ lib/maplibre-compose-material3/src/commonMain/kotlin/dev/sargunv/maplibrecompose/material3/controls/AttributionButton.kt
@@ -4,12 +4,14 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -23,21 +25,29 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.LinkAnnotation
 import androidx.compose.ui.text.TextLinkStyles
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withLink
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.PopupProperties
 import dev.sargunv.maplibrecompose.compose.CameraState
 import dev.sargunv.maplibrecompose.core.CameraMoveReason
 import dev.sargunv.maplibrecompose.core.source.AttributionLink
@@ -56,18 +66,34 @@ import org.jetbrains.compose.resources.stringResource
  *   [Source.attributionLinks][dev.sargunv.maplibrecompose.core.source.Source.attributionLinks] via
  *   [StyleState.sources][dev.sargunv.maplibrecompose.compose.StyleState.sources]
  * @param modifier the Modifier to be applied to this layout node
- * @param colors Colors that will be used for the info button
+ * @param iconColors Colors that will be used for the info button
  * @param textStyle Text style used for the attribution info
  * @param textLinkStyles Text link styles that should be used for the links in the attribution info
+ * @param popupEndPadding Padding that will be applied on the end of the popup (i.e. if the popup is
+ *   aligned to the start, padding will be applied on the end, but if the popup is aligned to the
+ *   end, padding will be applied to the start)
+ * @param popupShape Shape of the popup (applied to [Surface])
+ * @param popupColor Color of the popup (applied to [Surface])
+ * @param popupContentColor Content Color of the popup (applied to [Surface])
+ * @param popupTonalElevation Tonal Elevation of the popup (applied to [Surface])
+ * @param popupShadowElevation Shadow Elevation of the popup (applied to [Surface])
+ * @param popupBorder Border of the popup (applied to [Surface])
  */
 @Composable
 public fun AttributionButton(
   lastCameraMoveReason: CameraMoveReason,
   attributions: List<AttributionLink>,
   modifier: Modifier = Modifier,
-  colors: IconButtonColors = IconButtonDefaults.iconButtonColors(),
+  iconColors: IconButtonColors = IconButtonDefaults.iconButtonColors(),
   textStyle: TextStyle = MaterialTheme.typography.bodyMedium,
   textLinkStyles: TextLinkStyles? = null,
+  popupEndPadding: Dp = 0.dp,
+  popupShape: Shape = RoundedCornerShape(24.dp),
+  popupColor: Color = MaterialTheme.colorScheme.surface,
+  popupContentColor: Color = contentColorFor(popupColor),
+  popupTonalElevation: Dp = 0.dp,
+  popupShadowElevation: Dp = 0.dp,
+  popupBorder: BorderStroke? = null,
 ) {
   if (attributions.isEmpty()) return
 
@@ -81,7 +107,7 @@ public fun AttributionButton(
   }
 
   Box(modifier) {
-    IconButton(onClick = { expanded.targetState = !expanded.currentState }, colors = colors) {
+    IconButton(onClick = { expanded.targetState = !expanded.currentState }, colors = iconColors) {
       InfoIcon()
     }
 
@@ -89,22 +115,41 @@ public fun AttributionButton(
       // popup position provider places popup superimposed over the icon button, the arrangement
       // and alignment of its content depends on the position of the icon button in relation to the
       // screen.
+      val density = LocalDensity.current
       var alignLeft by remember { mutableStateOf(false) }
       var alignTop by remember { mutableStateOf(false) }
-      val popupPositionProvider = SuperimposingPopupPositionProvider { left, top ->
+      var popupWidth by remember { mutableStateOf(Dp.Unspecified) }
+      val popupPositionProvider = SuperimposingPopupPositionProvider { left, top, width ->
         alignLeft = left
         alignTop = top
+        popupWidth = with(density) { width.toDp() }
       }
       val verticalAlignment = if (alignTop) Alignment.Top else Alignment.Bottom
       val horizontalArrangement =
         if (alignLeft) Arrangement.Absolute.Left else Arrangement.Absolute.Reverse
+      val textAlign = if (alignLeft) TextAlign.Left else TextAlign.Right
+      val attributionAlignment = if (alignLeft) AbsoluteAlignment.Left else AbsoluteAlignment.Right
 
       Popup(
         popupPositionProvider = popupPositionProvider,
+        properties = PopupProperties(clippingEnabled = false),
         onDismissRequest = { expanded.targetState = false },
       ) {
-        AnimatedVisibility(visibleState = expanded, enter = fadeIn(), exit = fadeOut()) {
-          Surface(shape = RoundedCornerShape(24.dp)) {
+        AnimatedVisibility(
+          modifier =
+            Modifier.widthIfSpecified(popupWidth).paddingEndOfPopup(popupEndPadding, alignLeft),
+          visibleState = expanded,
+          enter = fadeIn(),
+          exit = fadeOut(),
+        ) {
+          Surface(
+            shape = popupShape,
+            color = popupColor,
+            contentColor = popupContentColor,
+            tonalElevation = popupTonalElevation,
+            shadowElevation = popupShadowElevation,
+            border = popupBorder,
+          ) {
             // the content of the popup should be aligned centered vertically in general, only the
             // icon button should be in the corner, so that it exactly overlaps the original button
             Row(
@@ -113,7 +158,7 @@ public fun AttributionButton(
             ) {
               IconButton(
                 onClick = { expanded.targetState = false },
-                colors = colors,
+                colors = iconColors,
                 modifier = Modifier.align(verticalAlignment),
               ) {
                 InfoIcon()
@@ -122,6 +167,8 @@ public fun AttributionButton(
                 attributions = attributions,
                 textStyle = textStyle,
                 textLinkStyles = textLinkStyles,
+                textAlign = textAlign,
+                attributionAlignment = attributionAlignment,
                 modifier = Modifier.padding(8.dp),
               )
               // icon buttons are automatically padded to have a certain click size, which makes the
@@ -134,6 +181,12 @@ public fun AttributionButton(
     }
   }
 }
+
+private fun Modifier.paddingEndOfPopup(padding: Dp, alignLeft: Boolean) =
+  if (alignLeft) absolutePadding(right = padding) else absolutePadding(left = padding)
+
+private fun Modifier.widthIfSpecified(width: Dp) =
+  if (width != Dp.Unspecified) width(width) else this
 
 @Composable
 private fun InfoIcon(modifier: Modifier = Modifier) {
@@ -150,16 +203,18 @@ private fun AttributionTexts(
   attributions: List<AttributionLink>,
   textStyle: TextStyle,
   textLinkStyles: TextLinkStyles?,
+  textAlign: TextAlign,
+  attributionAlignment: Alignment.Horizontal,
   modifier: Modifier = Modifier,
 ) {
   ProvideTextStyle(textStyle) {
-    FlowRow(modifier = modifier, horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+    FlowRow(modifier = modifier, horizontalArrangement = Arrangement.spacedBy(8.dp, attributionAlignment)) {
       attributions.distinct().forEach {
         val attributionString = buildAnnotatedString {
           val link = LinkAnnotation.Url(url = it.url, styles = textLinkStyles)
           withLink(link) { append(it.title) }
         }
-        Text(attributionString)
+        Text(text = attributionString, textAlign = textAlign)
       }
     }
   }
