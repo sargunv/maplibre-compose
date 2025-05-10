e8ccee71ea2da14b5fcb73a590a36440b0ac799c
Sargun Vohra <sargunv@users.noreply.github.com>
2025-05-11 01:02:12 +0200

    add remember fn

--- lib/maplibre-compose/src/commonMain/kotlin/dev/sargunv/maplibrecompose/core/source/TileSetOptions.kt
+++ lib/maplibre-compose/src/commonMain/kotlin/dev/sargunv/maplibrecompose/core/source/TileSetOptions.kt
@@ -5,8 +5,8 @@ import io.github.dellisd.spatialk.geojson.BoundingBox
 
 @Immutable
 public data class TileSetOptions(
-  val minZoom: Int = 0,
-  val maxZoom: Int = 18,
+  val minZoom: Int = Defaults.MIN_ZOOM,
+  val maxZoom: Int = Defaults.MAX_ZOOM,
   val tileCoordinateSystem: TileCoordinateSystem = TileCoordinateSystem.XYZ,
   val boundingBox: BoundingBox? = null,
   val attributionHtml: String? = null,
