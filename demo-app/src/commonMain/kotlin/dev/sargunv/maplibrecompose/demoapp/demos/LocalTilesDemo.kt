154f275295ce9234bc6ef526882519f70c0cb85f
Sargun Vohra <sargunv@users.noreply.github.com>
2025-05-12 00:28:41 +0200

    fix: configire tileset exceptions on ios

--- demo-app/src/commonMain/kotlin/dev/sargunv/maplibrecompose/demoapp/demos/LocalTilesDemo.kt
+++ demo-app/src/commonMain/kotlin/dev/sargunv/maplibrecompose/demoapp/demos/LocalTilesDemo.kt
@@ -2,35 +2,41 @@ package dev.sargunv.maplibrecompose.demoapp.demos
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import dev.sargunv.maplibrecompose.compose.MaplibreMap
 import dev.sargunv.maplibrecompose.compose.layer.RasterLayer
 import dev.sargunv.maplibrecompose.compose.rememberCameraState
 import dev.sargunv.maplibrecompose.compose.rememberStyleState
 import dev.sargunv.maplibrecompose.compose.source.rememberRasterSource
-import dev.sargunv.maplibrecompose.core.CameraPosition
 import dev.sargunv.maplibrecompose.core.source.TileSetOptions
 import dev.sargunv.maplibrecompose.demoapp.Demo
 import dev.sargunv.maplibrecompose.demoapp.DemoMapControls
 import dev.sargunv.maplibrecompose.demoapp.DemoOrnamentSettings
 import dev.sargunv.maplibrecompose.demoapp.DemoScaffold
+import dev.sargunv.maplibrecompose.demoapp.format
 import dev.sargunv.maplibrecompose.demoapp.generated.Res
-import io.github.dellisd.spatialk.geojson.Position
-
-private val NEW_YORK = Position(latitude = 40.744, longitude = -73.981)
+import kotlin.math.roundToInt
 
 object LocalTilesDemo : Demo {
-  // TODO spin this into a full local tiles demo
   override val name = "Tile Set"
-  override val description = "Configure a Tile Set programatically"
+  override val description = "Configure a Tile Set programatically with local tiles"
 
   @Composable
   override fun Component(navigateUp: () -> Unit) {
     DemoScaffold(this, navigateUp) {
       Column {
-        val cameraState =
-          rememberCameraState(CameraPosition(target = NEW_YORK, zoom = 15.0, tilt = 30.0))
+        val cameraState = rememberCameraState()
         val styleState = rememberStyleState()
 
         Box(modifier = Modifier.Companion.weight(1f)) {
@@ -42,26 +48,52 @@ object LocalTilesDemo : Demo {
           ) {
             val tiles =
               rememberRasterSource(
-                id = "osm-raster",
+                id = "fantasy-map",
                 tileSize = 256,
                 tiles =
+                  // TODO this is not working on Android atm (works on iOS)
                   listOf(
-                    "https://a.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                    "https://b.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                    "https://c.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                    Res.getUri("files/data/fantasy-map/0/0-0-fs8.png")
+                      .replace("0/0-0", "{z}/{x}-{y}")
                   ),
-                options =
-                  TileSetOptions(
-                    attributionHtml =
-                      "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors"
-                  ),
+                options = TileSetOptions(minZoom = 0, maxZoom = 4),
               )
 
-            RasterLayer(id = "tiles", source = tiles)
+            RasterLayer(id = "fantasy-map", source = tiles)
           }
           DemoMapControls(cameraState, styleState)
         }
+
+        Row(modifier = Modifier.safeDrawingPadding().wrapContentSize(Alignment.Center)) {
+          val pos = cameraState.position
+          val scale = cameraState.metersPerDpAtTarget
+
+          Cell("Latitude", pos.target.latitude.format(3), Modifier.weight(1.4f))
+          Cell("Longitude", pos.target.longitude.format(3), Modifier.weight(1.4f))
+          Cell("Zoom", pos.zoom.format(2), Modifier.weight(1f))
+          Cell("Bearing", pos.bearing.format(2), Modifier.weight(1f))
+          Cell("Tilt", pos.tilt.format(2), Modifier.weight(1f))
+          Cell("Scale", "${scale.roundToInt()}m", Modifier.weight(1f))
+        }
       }
     }
+  }
+}
+
+@Composable
+private fun Cell(title: String, value: String, modifier: Modifier = Modifier) {
+  Column(modifier = modifier.padding(PaddingValues(4.dp)).wrapContentSize(Alignment.Center)) {
+    Text(
+      text = title,
+      textAlign = TextAlign.Center,
+      maxLines = 1,
+      style = MaterialTheme.typography.labelSmall,
+    )
+    Text(
+      text = value,
+      textAlign = TextAlign.Center,
+      maxLines = 1,
+      style = MaterialTheme.typography.bodySmall,
+    )
   }
 }
